9fd942bcdfc3d2f5aed42b0e1123c6ababd56b12
Austin Leuenhagen <42649201+Lui798@users.noreply.github.com>
2018-10-08 11:19:08 -0500

    started card and blackjack

---
+++ CardGame/CardGame/Blackjack.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <random>
+#include <Windows.h>
+#include "Card.h"
+
+void blackjack()
+{
+	std::cout << "Blackjack";
+	system("pause");
+}
---
+++ CardGame/CardGame/Card.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef CARD_H
+#define CARD_H
+
+class Card {};
+
+#endif
